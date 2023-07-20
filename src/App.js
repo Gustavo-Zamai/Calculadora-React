5205c24d85c72bcaad4e95f45570e30c713d57d9
Gustavo-Zamai <gustavosimaozamai@gmail.com>
2023-07-20 00:28:42 -0300

    Add subtraction function

--- src/App.js
+++ src/App.js
@@ -31,14 +31,30 @@ const App = () => {
     }
   }
 
+  const handleMinusNumbers = () => {
+    if (firstNumber === '0') {
+      setFirstNumber(String(currentNumber));
+      setCurrentNumber('0');
+      setOperation('-');
+    } else {
+      const minus = Number(firstNumber) - Number(currentNumber);
+      setCurrentNumber(String(minus));
+      setOperation('');
+    }
+  }
+
+
   const handleEquals = () => {
     if (firstNumber !== '0' && operation !== '' && currentNumber !== '0') {
       switch (operation) {
         case '+':
           handleSumNumbers();
           break;
-
+        case '-':
+          handleMinusNumbers();
+          break;
         default:
+
           break;
       }
     }
@@ -58,7 +74,7 @@ const App = () => {
           <Button label='7' onClick={() => handleAddNumber('7')} />
           <Button label='8' onClick={() => handleAddNumber('8')} />
           <Button label='9' onClick={() => handleAddNumber('9')} />
-          <Button label='-' onClick={() => handleAddNumber('')} />
+          <Button label='-' onClick={handleMinusNumbers} />
         </Row>
         <Row>
           <Button label='4' onClick={() => handleAddNumber('4')} />
